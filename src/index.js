59b4e2f53bc95899e504b4c54ef574a0ea2b581a
aryelciu001 <aryelciu001@gmail.com>
2020-08-13 00:06:08 +0800

    Added Environment Variables

--- src/index.js
+++ src/index.js
@@ -8,23 +8,21 @@ require('firebase/firestore');
 
 // Your web app's Firebase configuration 
 // Initialize Firebase
-// let apiKey = process.env.REACT_APP_API_KEY
-// let apiKey = process.env.REACT_APP_API_KEY
-// let apiKey = process.env.REACT_APP_AUTH_DOMAIN
-// let apiKey = process.env.REACT_APP_DB_URL
-// let apiKey = process.env.REACT_APP_PROJECT_ID
-// process.env.REACT_APP_STORAGE_BUCKET
-// process.env.REACT_APP_SENDER_ID
-// process.env.REACT_APP_APP_ID
+let apiKey = process.env.REACT_APP_API_KEY
+let authDomain = process.env.REACT_APP_AUTH_DOMAIN
+let databaseURL = process.env.REACT_APP_DB_URL
+let storageBucket = process.env.REACT_APP_STORAGE_BUCKET
+let messagingSenderId = process.env.REACT_APP_SENDER_ID
+let appId = process.env.REACT_APP_APP_ID
 firebase.initializeApp(
   {
-    apiKey: AIzaSyAAxZaHlIia7KRBhMom9Ddfx3GYzWuHgiM,
-    authDomain: "evernoteclone-1b3dd.firebaseapp.com",
-    databaseURL: "https://evernoteclone-1b3dd.firebaseio.com",
+    apiKey,
+    authDomain,
+    databaseURL,
     projectId: "evernoteclone-1b3dd",
-    storageBucket: "evernoteclone-1b3dd.appspot.com",
-    messagingSenderId: "651281430873",
-    appId: "1:651281430873:web:13beeaf0de67ab9109b448"
+    storageBucket,
+    messagingSenderId,
+    appId
   }
 );
 
